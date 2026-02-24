9aa328dc49a4f0c258da098ad11aabedbc2c73a1
Malformed Aluminum Alloy <milo.sanokklis@mymail.champlain.edu>
2026-02-24 16:13:34 -0500

    Add PowerShell script for managing AD OUs

---
+++ powershell/ad_add.ps1
@@ -0,0 +1,13 @@
+$oupath = "DC=milo,DC=local"
+$allpath = "OU=TESTING,DC=milo,DC=local"
+$delpath = "OU=Test OU,DC=milo,DC=local"
+
+New-ADOrganizationalUnit -Name "TESTING" -Path $oupath -ProtectedFromAccidentalDeletion $false
+
+Get-ADUser -Identity "milos" | Move-ADObject -TargetPath $allpath
+
+Get-ADUser -Identity "WKS01-MILO" | Move-ADObject -TargetPath $allpath
+
+Set-ADObject -Identity $delpath -ProtectedFromAccidentalDeletion $false
+
+Remove-ADOrganizationalUnit -Identity $delpath -Recursive -Confirm:$False
